336220e5c84b550efa76681fb47ce9a9a49c22c7
Priyadarshan Garg <gargpriyadarshan18@gmail.com>
2026-03-27 21:12:30 +0530

    BlockingQueue impl from scartch

---
+++ src/threading/BoundedBlockingQueue.java
@@ -0,0 +1,62 @@
+package threading;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BoundedBlockingQueue<T> {
+
+    private final int capacity;
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition notFull = lock.newCondition();
+    private final Condition notEmpty = lock.newCondition();
+    private final Queue<T> queue = new LinkedList<>();
+    public BoundedBlockingQueue(int capacity) {
+        if (capacity <= 0) throw new IllegalArgumentException("Capacity must be > 0");
+        this.capacity = capacity;
+    }
+    public void enque(T element) {
+        lock.lock();
+        try {
+            while(queue.size() == capacity) {
+                notFull.await(); // bhai full hai me lock kholke waiting room me jaa rha hu
+            }
+            queue.add(element);
+            notEmpty.signal(); // mene daal diya ab consumer aake le ja sakta hai
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    public  T deque () {
+       lock.lock();
+        try{
+            while (queue.isEmpty()) {
+                notEmpty.await(); // khaali hai to consumer notEmpty ke waiting room me
+            }
+            T element = queue.remove();
+            notFull.signal(); // ab ye khaali to producer isme daal de
+            return element;
+        }
+      catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int size() {
+       lock.lock();
+       try {
+           return queue.size();
+       }
+       finally {
+           lock.unlock();
+       }
+    }
+}
