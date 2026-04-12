4a5faebe0cfe22a6db0369092bceada840c6779c
Priyadarshan Garg <gargpriyadarshan18@gmail.com>
2026-04-12 12:25:54 +0530

    notify and natoufyAll ka difference

--- src/threading/PingPongIntermediate.java
+++ src/threading/PingPongIntermediate.java
@@ -2,58 +2,34 @@ package threading;
 
 public class PingPongIntermediate {
     private static int limit;
-    public int count = 0;
+    public int count = 1;
 
     public PingPongIntermediate(int number) {
         limit = number;
     }
 
-    public synchronized void printNumber() {
+    public synchronized void printNumber(int myTurn) {
         while (count <= limit) {
             int rem = count % 3;
-            if (rem == 0) {
-                if (Thread.currentThread().getName().equals("three")) {
-                    System.out.println("Printed by : " + Thread.currentThread().getName() + " " + count++);
-                    notifyAll();
-                } else {
-                    try {
-                        wait();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            } else if (rem == 1) {
-                if (Thread.currentThread().getName().equals("one")) {
-                    System.out.println("Printed by : " + Thread.currentThread().getName() + " " + count++);
-                    notifyAll();
-                } else {
-                    try {
-                        wait();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            } else {
-                if (Thread.currentThread().getName().equals("two")) {
-                    System.out.println("Printed by : " + Thread.currentThread().getName() + " " + count++);
-                    notifyAll();
-                } else {
-                    try {
-                        wait();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+            if(rem == myTurn) {
+                System.out.println("Printed by : " + Thread.currentThread().getName() + " " + count++);
+                notifyAll();
+            }
+            else {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
-
         }
     }
 
     public static void main(String[] args) {
         PingPongIntermediate pingPongIntermediate = new PingPongIntermediate(25);
-        Thread one = new Thread(pingPongIntermediate::printNumber);
-        Thread two = new Thread(pingPongIntermediate::printNumber);
-        Thread three = new Thread(pingPongIntermediate::printNumber);
+        Thread one = new Thread(()->pingPongIntermediate.printNumber(1));
+        Thread two = new Thread(()->pingPongIntermediate.printNumber(2));
+        Thread three = new Thread(()->pingPongIntermediate.printNumber(0));
         one.setName("one");
         two.setName("two");
         three.setName("three");
